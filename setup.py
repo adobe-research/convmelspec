31004a112055e10467f2f1adadd1ee18fcfdf96d
Nikhil Kalra <nikalra@adobe.com>
2023-06-26 13:29:24 -0700

    bump torch

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     install_requires=[
         "numpy",
         "scipy",
-        "torch",
+        "torch>=2.0.1",
         "soundfile",
         "coremltools>=7.0b1",
         "onnxruntime",
